1fe3053cd118f96b08a9302108b921454575db62
Jessie Wong <kimjang.wong1@gmail.com>
2021-03-02 11:25:34 +0000

    wks code updated

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const http = require('http');
 const axios = require('axios');
+const logger = require('morgan');
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-http.createServer((req, res)=>{
-  res.write(users.join(", ")); //display the list of users on the page
-  res.write("\n\n"+emails.join(", ")); //display the list of users on the page
-  res.end(); //end the response
-}).listen(8000); // listen for requests on port 8000
+var app = express();
+var port = 8000;
+
+app.use(bodyParser.json())
+
+app.get('/hello/:foo/:bar', (req, res) => {
+    res.json({message: "Hello!!", data: [
+        req.params.foo,
+        res.params.bar
+    ]});
+});
+
+//http.createServer((req, res)=>{
+//  res.write(users.join(", ")); //display the list of users on the page
+//  res.write("\n\n"+emails.join(", ")); //display the list of users on the page
+//  res.end(); //end the response
+//}).listen(8000); // listen for requests on port 8000
 
 let users = []; // names of users will be stored here
 let email = [];
@@ -19,4 +35,8 @@ let email = [];
   } catch(error){
     console.log(error)
   }
-})()
+})();
+
+app.listen(port, function(err){
+    console.log('Listening on port: ' + port);
+});
